e29d4f71637a615b3f57a910ce6a6b43445b5b5e
Luís Santos <fc56341@alunos.fc.ul.pt>
2025-04-26 20:37:28 +0100

    all solver bugs fixed

--- z3_prototype/test.py
+++ z3_prototype/test.py
@@ -25,12 +25,12 @@ def add_level_constraint(solver, z3_consts, solver_constraints, l1, l2, name):
 
     solver.assert_and_track(constraint, name)  
     solver_constraints.append(constraint)
+    return constraint
 
 def check_inequalities(inequalities, file_path):
     solver = Solver()
     z3_consts = {}
     constraint_map = {}
-    constraints = []
     solver_constraints = []  
 
     truths = [
@@ -41,67 +41,53 @@ def check_inequalities(inequalities, file_path):
     solver.add(*truths)
     solver_constraints.extend(truths)
 
-    # truth1 = ForAll([Const('x', Levels)], bot < value(Const('x', Levels)))
-    # truth2 = ForAll([Const('x', Levels)], value(Const('x', Levels)) < top)
-    # truth3 = bot < top
-    # solver.add(truth1)
-    # solver.add(truth2)
-    # solver.add(truth3)
-    # solver_constraints.append(truth1)
-    # solver_constraints.append(truth2)
-    # solver_constraints.append(truth3)
-
     for i, ineq in enumerate(inequalities):
         span = ineq["span"]
         l1 = ineq["l1"]
         l2 = ineq["l2"]
-        name = f"constraint_{i}"
-        constraint_map[name] = {"span": span, "l1": l1, "l2": l2}
-        add_level_constraint(solver, z3_consts, solver_constraints, l1, l2, name)
-        constraints.append((name, l1, l2))
+        constraint_id = f"constraint_{i}"
+        constraint = add_level_constraint(solver, z3_consts, solver_constraints, l1, l2, constraint_id)
+        constraint_map[constraint_id] = {"span": span, "l1": l1, "l2": l2, "constraint": constraint}
 
+    unsat_constraints = []
     if solver.check() == sat:
         return []
     else:
-        unsat_core = solver.unsat_core()
-        unsat_constraints = [
-            {
-                "span": constraint_map[str(c)]["span"],
-                "l1": constraint_map[str(c)]["l1"],
-                "l2": constraint_map[str(c)]["l2"],
-                "file_path": file_path,
-            }
-            for c in unsat_core
-        ]
+        while solver.check() == unsat:
+            unsat_core = solver.unsat_core()
+            unsat_constraints.extend([
+                {
+                    "span": constraint_map[str(c)]["span"],
+                    "l1": constraint_map[str(c)]["l1"],
+                    "l2": constraint_map[str(c)]["l2"],
+                    "file_path": file_path,
+                }
+                for c in unsat_core
+            ])
 
-        for constraint in constraints:
-            if str(constraint[0]) in [str(c) for c in unsat_core]:
-                constraint = get_val(constraint[1]) < get_val(constraint[2])
-                solver = rebuild_solver_without_constraint(solver, solver_constraints, constraint)
-                solver_constraints.remove(constraint)
-
-        for name, l1, l2 in constraints:
-            if name not in [str(c) for c in unsat_core]:
-                # print(f"Checking constraint: {name}, {l1}, {l2}")
-                constraint = get_val(l1) < get_val(l2)
-                new_solver = rebuild_solver_without_constraint(solver, solver_constraints, constraint)
-                if solver.check() == unsat and new_solver.check() == sat:
-                    unsat_constraints.append({
-                        "span": constraint_map[name]["span"],
-                        "l1": constraint_map[name]["l1"],
-                        "l2": constraint_map[name]["l2"],
-                        "file_path": file_path,
-                    })
+            for constraint_id in constraint_map.keys():
+                if constraint_id in [str(c) for c in unsat_core]:
+                    c = constraint_map[constraint_id]["constraint"]
+                    solver = rebuild_solver_without_constraint(constraint_map, solver_constraints, c, truths)
+                    solver_constraints.remove(c)
 
         return unsat_constraints
-    
-def rebuild_solver_without_constraint(original_solver, all_constraints, constraint_to_remove):
+
+def rebuild_solver_without_constraint(constraint_map, solver_constraints, constraint_to_remove, truths):
     new_solver = Solver()
-    for constraint in all_constraints:
-        if str(constraint) != str(constraint_to_remove):
-            new_solver.add(constraint)
+    for constraint in solver_constraints:
+        if str(constraint) != str(constraint_to_remove): 
+            if str(constraint) in [str(c) for c in truths]:
+                new_solver.add(constraint)
+            else:
+                new_solver.assert_and_track(constraint, get_constraint_id(constraint, constraint_map))
     return new_solver
 
+def get_constraint_id(constraint, constraint_map):
+    for key, value in constraint_map.items():
+        if str(value["constraint"]) == str(constraint):
+            return key
+    
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     json_files = [f for f in os.listdir(script_dir) if f.endswith(".json")]
